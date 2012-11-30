d8d333622d9f12c72ee69ab09ee7e14a5221102a
Diego Felix <diegoflx.oliveira@gmail.com>
2012-11-30 17:22:35 -0200

    changed the tests

--- tests/general.test.php
+++ tests/general.test.php
@@ -3,25 +3,25 @@ class TestGeneral extends PHPUnit_Framework_TestCase {
     
     public function testRelativeTimeIsCreated()
     {
-        Bundle::start('relative');
-        $this->assertEquals(Relative::get(), 'just now');
+        Bundle::start('relativetime');
+        $this->assertEquals(RelativeTime::get(), 'just now');
     }
 
     public function testRelativeTimeIsCreatedWithDate()
     {
-        Bundle::start('relative');
+        Bundle::start('relativetime');
 
         $date = date('Y-m-d H:i:s', time() - 36000);
 
-        $this->assertEquals(Relative::get($date), '10 hours ago');
+        $this->assertEquals(RelativeTime::get($date), '10 hours ago');
     }
 
     public function testRelativeTimeIsCreatedWithTime()
     {
-        Bundle::start('relative');
+        Bundle::start('relativetime');
 
         $time = time() - 36000;
 
-        $this->assertEquals(Relative::get($time), '10 hours ago');
+        $this->assertEquals(RelativeTime::get($time), '10 hours ago');
     }
 }
